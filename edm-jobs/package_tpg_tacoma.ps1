e28300a27a4f8957ce33ab60e2d5156a9b491648
Thepropergrip <huntjlc@gmail.com>
2026-09-03 16:24:53 -0400

    Rebuild Tacoma registration around proven Currenthill-style DCS vehicle loader

--- edm-jobs/package_tpg_tacoma.ps1
+++ edm-jobs/package_tpg_tacoma.ps1
@@ -41,105 +41,158 @@ if (-not (Get-ChildItem $textures -File -ErrorAction SilentlyContinue)) {
 $entry = @'
 declare_plugin("TPG Tacoma Recon",
 {
-    installed = true,
-    dirName = current_mod_path,
-    displayName = _("TPG Tacoma Recon"),
-    version = "1.0.0",
-    state = "installed",
-    info = _("2016 Toyota Tacoma TRD Off Road 4x4 DCLB custom scout/recon vehicle")
+    dirName       = current_mod_path,
+    displayName   = _("TPG Tacoma Recon"),
+    shortName     = "TPG_TACOMA",
+    version       = "1.0.2",
+    state         = "installed",
+    installed     = true,
+    developerName = "TPG",
+    info          = _("2016 Toyota Tacoma TRD Off Road 4x4 DCLB custom scout/recon vehicle"),
 })
 
-mount_vfs_model_path(current_mod_path.."/Shapes")
-mount_vfs_texture_path(current_mod_path.."/Textures")
+mount_vfs_model_path   (current_mod_path .. "/Shapes")
+mount_vfs_texture_path (current_mod_path .. "/Textures")
 
-dofile(current_mod_path.."/Database/db_tpg_tacoma_recon.lua")
+dofile(current_mod_path .. "/Database/db_vehicles.lua")
 
 plugin_done()
 '@
 Set-Content -Path (Join-Path $pkg "entry.lua") -Value $entry -Encoding UTF8
 
-$dbLua = @'
+
+$chassisDir = Join-Path $db "Chassis"
+$vehicleDir = Join-Path $db "Vehicles"
+New-Item -ItemType Directory -Force -Path $chassisDir,$vehicleDir | Out-Null
+
+$chassisLua = @'
+GT_t.CH_t.tpg_tacoma_chassis = {
+    life = 4.0,
+    mass = 2500,
+    length = 5.728,
+    width = 1.895,
+    max_road_velocity = 50.5156,
+    max_slope = 0.47,
+    canSwim = false,
+    canWade = true,
+    engine_power = 210,
+    engineMinRPM = 600,
+    engineMaxPowerRPM = 4600,
+    engineMaxRPM = 6200,
+    gearRatios = { -3.52, 0.0, 3.60, 2.09, 1.49, 1.00, 0.69, 0.58 },
+    mainGearRatio = 3.91,
+    automaticTransmission = true,
+    max_vert_obstacle = 0.35,
+    max_acceleration = 4.2,
+    min_turn_radius = 5.8,
+    X_gear_1 = 1.7855,
+    Y_gear_1 = 0,
+    Z_gear_1 = 0.405,
+    X_gear_2 = -1.7855,
+    Y_gear_2 = 0,
+    Z_gear_2 = 0.405,
+    gear_type = GT_t.GEAR_TYPES.WHEELS,
+    r_max = 0.405,
+    trace_width = 0.285,
+    armour_thickness = 0.001,
+}
+'@
+Set-Content -Path (Join-Path $chassisDir "tpg_tacoma_chassis.lua") -Value $chassisLua -Encoding UTF8
+
+$vehicleLua = @'
 GT = {};
 GT_t.ws = 0;
 
--- Start from a DCS-native wheeled chassis template so all required inherited
--- tables exist before we override Tacoma-specific dimensions/performance.
 set_recursive_metatable(GT, GT_t.generic_wheel_vehicle);
-set_recursive_metatable(GT.chassis, GT_t.CH_t.KAMAZ43101);
-
-GT.chassis.life = 4.0;
-GT.chassis.mass = 2500;
-GT.chassis.length = 5.728;
-GT.chassis.width = 1.895;
-GT.chassis.max_road_velocity = 50.5156; -- 113 mph / 181.856 km/h
-GT.chassis.max_slope = 0.55;
-GT.chassis.engine_power = 21000;
-GT.chassis.gear_count = 6;
-GT.chassis.canSwim = false;
-GT.chassis.canWade = true;
-GT.chassis.waterline_level = 0.20;
-GT.chassis.fordingDepth = 0.55;
-GT.chassis.max_vert_obstacle = 0.35;
-GT.chassis.max_acceleration = 4.2;
-GT.chassis.min_turn_radius = 5.8;
-GT.chassis.X_gear_1 = 1.7855;
-GT.chassis.Y_gear_1 = 0;
-GT.chassis.Z_gear_1 = 0.405;
-GT.chassis.X_gear_2 = -1.7855;
-GT.chassis.Y_gear_2 = 0;
-GT.chassis.Z_gear_2 = 0.405;
-GT.chassis.r_max = 0.405;
-GT.chassis.armour_thickness = 0.002;
+set_recursive_metatable(GT.chassis, GT_t.CH_t.tpg_tacoma_chassis);
 
 GT.visual.shape = "TPG_Tacoma_Recon";
 GT.visual.shape_dstr = "TPG_Tacoma_Recon_Destroyed";
 
+GT.chassis.life = 4.0;
 GT.swing_on_run = false;
-GT.mobile = true;
-GT.Crew = 1;
-GT.armour_scheme = unarmed_armour_scheme;
 
--- DCS native wheel arguments for wheeled vehicles.
-GT.animation_arguments.wheels_rotation = 8;
-GT.animation_arguments.wheels_turn_angle = 9;
-
-GT.sensor = {};
-set_recursive_metatable(GT.sensor, GT_t.SN_visual);
-GT.sensor.height = 1.75;
-
-GT.visual.fire_size = 0.65;
+GT.visual.fire_size = 0.25;
 GT.visual.fire_pos[1] = 0.0;
 GT.visual.fire_pos[2] = 0.75;
 GT.visual.fire_pos[3] = 0.0;
-GT.visual.fire_time = 420;
-GT.time_agony = 12;
-
-GT.driverViewPoint = {0.72, 1.55, -0.32};
-GT.CustomAimPoint = {0.0, 1.15, 0.0};
+GT.visual.fire_time = 360;
+GT.visual.min_time_agony = 5;
+GT.visual.max_time_agony = 10;
 
 GT.Name = "TPG_Tacoma_Recon";
-GT.DisplayName = _("Scout/Recon - 2016 Tacoma TRD Off Road DCLB");
-GT.Rate = 8;
+GT.DisplayName = _("TPG Tacoma Scout/Recon");
+GT.DisplayNameShort = _("Tacoma Recon");
+GT.Rate = 5;
 
-GT.DetectionRange = 5000;
+GT.DetectionRange = 0;
 GT.ThreatRange = 0;
-GT.mapclasskey = "P0091000005";
+GT.mapclasskey = "P0091000212";
 
--- Use the same proven ME classification pattern as DCS unarmed trucks.
 GT.attribute = {
     wsType_Ground,
     wsType_Tank,
     wsType_NoWeapon,
-    wsTypeKAMAZ_Tent,
+    wsType_GenericVehicle,
     "Trucks",
 };
 
 GT.category = "Unarmed";
-GT.MaxSpeed = 181.856;
 
-add_surface_unit(GT);
+GT.tags = {
+    "Unarmed",
+    "Scout/Recon",
+};
+
+GT.Countries = {"USA"};
 '@
-Set-Content -Path (Join-Path $db "db_tpg_tacoma_recon.lua") -Value $dbLua -Encoding UTF8
+Set-Content -Path (Join-Path $vehicleDir "tpg_tacoma.lua") -Value $vehicleLua -Encoding UTF8
+
+$dbLua = @'
+local plugin_db_path = current_mod_path .. "/Database/"
+
+local function chassis_file(f)
+    if dofile(plugin_db_path .. f) then
+        error("can't load file " .. f)
+        return
+    end
+end
+
+local function vehicle_file(f)
+    if dofile(plugin_db_path .. f) then
+        error("can't load file " .. f)
+        return
+    end
+    if GT then
+        GT.shape_table_data =
+        {
+            {
+                file        = GT.visual.shape;
+                username    = GT.Name;
+                desrt       = GT.visual.shape_dstr;
+                classname   = "lLandVehicle";
+                positioning = "BYNORMAL";
+                life        = GT.life or 6;
+            },
+            {
+                name = GT.visual.shape_dstr;
+                file = GT.visual.shape_dstr;
+            },
+        }
+        GT.MaxSpeed = GT.chassis and GT.chassis.max_road_velocity and (GT.chassis.max_road_velocity * 3.6)
+        add_surface_unit(GT)
+        GT = nil;
+    else
+        error("GT empty in file " .. f)
+    end
+end
+
+GT = nil;
+chassis_file("Chassis/tpg_tacoma_chassis.lua")
+vehicle_file("Vehicles/tpg_tacoma.lua")
+'@
+Set-Content -Path (Join-Path $db "db_vehicles.lua") -Value $dbLua -Encoding UTF8
+
 
 $readme = @'
 TPG Tacoma Recon v1.0
