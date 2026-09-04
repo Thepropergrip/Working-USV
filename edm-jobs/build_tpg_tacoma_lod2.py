2a6a02d488acb40e3dbb82d729e69b15ce2f5212
Thepropergrip <huntjlc@gmail.com>
2026-09-04 14:37:08 -0400

    Route Tacoma LOD2 through hero-body patch6

--- edm-jobs/build_tpg_tacoma_lod2.py
+++ edm-jobs/build_tpg_tacoma_lod2.py
@@ -2,4 +2,4 @@ import os, runpy
 runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
 os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="2"
-runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch5.py", run_name="__main__")
+runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch6.py", run_name="__main__")
