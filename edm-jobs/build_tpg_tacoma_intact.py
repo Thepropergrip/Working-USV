95794816e9ecf9c02fecc1bf56a85ad1c3993a37
Thepropergrip <huntjlc@gmail.com>
2026-09-04 09:04:28 -0400

    Use corrected wheel-rig Tacoma quality patch

--- edm-jobs/build_tpg_tacoma_intact.py
+++ edm-jobs/build_tpg_tacoma_intact.py
@@ -3,4 +3,4 @@ import os, runpy
 runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
 os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="0"
-runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch.py", run_name="__main__")
+runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch2.py", run_name="__main__")
