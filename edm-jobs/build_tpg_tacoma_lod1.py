72b3c92318d2e909cb44f6b76c3e8bb86df555a5
Thepropergrip <huntjlc@gmail.com>
2026-09-05 03:58:07 -0400

    Wire Tacoma LOD1 export to patch20

--- edm-jobs/build_tpg_tacoma_lod1.py
+++ edm-jobs/build_tpg_tacoma_lod1.py
@@ -2,4 +2,4 @@ import os, runpy
 runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
 os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="1"
-runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch19.py", run_name="__main__")
+runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch20.py", run_name="__main__")
