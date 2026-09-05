39b1bf6c037907621913a9fc26a6b2628fc5ac22
Thepropergrip <huntjlc@gmail.com>
2026-09-05 10:50:06 -0400

    Rebuild LIGHTS destroyed model without active fixtures

--- edm-jobs/build_tpg_substation_lights_destroyed.py
+++ edm-jobs/build_tpg_substation_lights_destroyed.py
@@ -1,4 +1,4 @@
 import os, runpy
-os.environ["TPG_SUB_DESTROYED"]="1"
-os.environ["TPG_SUB_LOD"]="0"
-runpy.run_path("edm-jobs/build_tpg_substation_lights_common.py", run_name="__main__")
+os.environ["TPG_SUB_DESTROYED"] = "1"
+os.environ["TPG_SUB_LOD"] = "0"
+runpy.run_path("edm-jobs/build_tpg_substation.py", run_name="__main__")
