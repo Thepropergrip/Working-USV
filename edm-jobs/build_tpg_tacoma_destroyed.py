339b44688e9ba6f0da7ad4592037a4d1253758cf
Thepropergrip <huntjlc@gmail.com>
2026-09-05 11:11:20 -0400

    Use FBX payload body for Tacoma destroyed export

--- edm-jobs/build_tpg_tacoma_destroyed.py
+++ edm-jobs/build_tpg_tacoma_destroyed.py
@@ -2,4 +2,4 @@ import os, runpy
 runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
 os.environ["TPG_TACOMA_DESTROYED"]="1"
 os.environ["TPG_TACOMA_LOD"]="0"
-runpy.run_path("edm-jobs/build_tpg_tacoma_rebuild_v1.py", run_name="__main__")
+runpy.run_path("edm-jobs/build_tpg_tacoma.py", run_name="__main__")
