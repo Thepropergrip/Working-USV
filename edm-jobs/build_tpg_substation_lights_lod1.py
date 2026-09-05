ad07541cb232b51e371d41c302df9c3798091ad9
Thepropergrip <huntjlc@gmail.com>
2026-09-05 10:49:19 -0400

    Rebuild LIGHTS LOD1 with projector connectors

--- edm-jobs/build_tpg_substation_lights_lod1.py
+++ edm-jobs/build_tpg_substation_lights_lod1.py
@@ -1,4 +1,5 @@
 import os, runpy
-os.environ["TPG_SUB_DESTROYED"]="0"
-os.environ["TPG_SUB_LOD"]="1"
-runpy.run_path("edm-jobs/build_tpg_substation_lights_common.py", run_name="__main__")
+os.environ["TPG_SUB_DESTROYED"] = "0"
+os.environ["TPG_SUB_LOD"] = "1"
+runpy.run_path("edm-jobs/build_tpg_substation.py", run_name="__main__")
+runpy.run_path("edm-jobs/add_tpg_substation_projector_connectors.py", run_name="__main__")
