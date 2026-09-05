101c88a4d95c583f1070287c2078c2870925c7f3
Thepropergrip <huntjlc@gmail.com>
2026-09-05 17:38:16 -0400

    Instrument Tacoma intact build with source-mesh bounds

--- edm-jobs/build_tpg_tacoma_intact.py
+++ edm-jobs/build_tpg_tacoma_intact.py
@@ -4,5 +4,6 @@ runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
 os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="0"
 runpy.run_path("edm-jobs/build_tpg_tacoma.py", run_name="__main__")
+runpy.run_path("edm-jobs/diagnose_tpg_tacoma_source_mesh.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_clean_rebuild_v13.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_wheel_closeout.py", run_name="__main__")
