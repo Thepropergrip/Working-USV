8120c07fe52367cc54a679c82f82cafeabd51def
Thepropergrip <huntjlc@gmail.com>
2026-09-04 21:00:40 -0400

    Route Tacoma LOD1 build through patch10

--- edm-jobs/build_tpg_tacoma_lod1.py
+++ edm-jobs/build_tpg_tacoma_lod1.py
@@ -2,4 +2,4 @@ import os, runpy
 runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
 os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="1"
-runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch9.py", run_name="__main__")
+runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch10.py", run_name="__main__")
