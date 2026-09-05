831f8cdf448cfdddc5b822db930e63f316ece63c
Thepropergrip <huntjlc@gmail.com>
2026-09-05 19:42:06 -0400

    Wire Tacoma V14 photo-match into LOD2

--- edm-jobs/build_tpg_tacoma_lod2.py
+++ edm-jobs/build_tpg_tacoma_lod2.py
@@ -4,4 +4,5 @@ os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="2"
 runpy.run_path("edm-jobs/build_tpg_tacoma.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_clean_rebuild_v13.py", run_name="__main__")
+runpy.run_path("edm-jobs/build_tpg_tacoma_photo_match_v14.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_wheel_closeout.py", run_name="__main__")
