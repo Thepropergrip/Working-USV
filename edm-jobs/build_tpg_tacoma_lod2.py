f329f0100c05a954e5cff3fa5d6c6b4fcf0fb892
Thepropergrip <huntjlc@gmail.com>
2026-09-05 10:32:40 -0400

    Route Tacoma LOD2 export to clean rebuild v1

--- edm-jobs/build_tpg_tacoma_lod2.py
+++ edm-jobs/build_tpg_tacoma_lod2.py
@@ -1,5 +1,5 @@
 import os, runpy
 runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
-os.environ["TPG_TACOMA_DESTROYED"] = "0"
-os.environ["TPG_TACOMA_LOD"] = "2"
-runpy.run_path("edm-jobs/build_tpg_tacoma_quality_patch28.py", run_name="__main__")
+os.environ["TPG_TACOMA_DESTROYED"]="0"
+os.environ["TPG_TACOMA_LOD"]="2"
+runpy.run_path("edm-jobs/build_tpg_tacoma_rebuild_v1.py", run_name="__main__")
