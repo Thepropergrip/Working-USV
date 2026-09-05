f9a1afcbb7ffd961d76d68183dcfe95d253545bc
Thepropergrip <huntjlc@gmail.com>
2026-09-05 12:25:49 -0400

    Apply Tacoma hero topper correction to intact export

--- edm-jobs/build_tpg_tacoma_intact.py
+++ edm-jobs/build_tpg_tacoma_intact.py
@@ -5,3 +5,4 @@ runpy.run_path("edm-jobs/prepare_tpg_tacoma_payload.py", run_name="__main__")
 os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="0"
 runpy.run_path("edm-jobs/build_tpg_tacoma.py", run_name="__main__")
+runpy.run_path("edm-jobs/apply_tpg_tacoma_hero_body_v4.py", run_name="__main__")
