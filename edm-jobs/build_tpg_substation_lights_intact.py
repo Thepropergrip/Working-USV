9bc516aa89dab53bbccd94c88dd82943b2267538
Thepropergrip <huntjlc@gmail.com>
2026-09-05 15:56:29 -0400

    Apply ground bed and brick upgrade to LIGHTS intact

--- edm-jobs/build_tpg_substation_lights_intact.py
+++ edm-jobs/build_tpg_substation_lights_intact.py
@@ -2,4 +2,5 @@ import os, runpy
 os.environ["TPG_SUB_DESTROYED"] = "0"
 os.environ["TPG_SUB_LOD"] = "0"
 runpy.run_path("edm-jobs/build_tpg_substation.py", run_name="__main__")
+runpy.run_path("edm-jobs/postprocess_tpg_substation_surface_upgrade.py", run_name="__main__")
 runpy.run_path("edm-jobs/add_tpg_substation_projector_connectors.py", run_name="__main__")
