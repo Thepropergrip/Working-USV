18df7ee6938d4928e4df4604ffcab7f0e17f6ed4
Thepropergrip <huntjlc@gmail.com>
2026-09-06 09:20:50 -0400

    Apply supplied asset visual upgrades to destroyed LIGHTS build

--- edm-jobs/build_tpg_substation_lights_destroyed.py
+++ edm-jobs/build_tpg_substation_lights_destroyed.py
@@ -1,5 +1,5 @@
 import os, runpy
 os.environ["TPG_SUB_DESTROYED"] = "1"
 os.environ["TPG_SUB_LOD"] = "0"
-runpy.run_path("edm-jobs/build_tpg_substation.py", run_name="__main__")
+runpy.run_path("edm-jobs/run_tpg_substation_user_asset_build.py", run_name="__main__")
 runpy.run_path("edm-jobs/postprocess_tpg_substation_surface_upgrade.py", run_name="__main__")
