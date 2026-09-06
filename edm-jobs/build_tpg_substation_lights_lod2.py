c51a5322e5b7fe523a7c2ae230e52b715e44f23c
Thepropergrip <huntjlc@gmail.com>
2026-09-06 09:20:43 -0400

    Apply supplied asset visual upgrades to LIGHTS LOD2

--- edm-jobs/build_tpg_substation_lights_lod2.py
+++ edm-jobs/build_tpg_substation_lights_lod2.py
@@ -1,6 +1,6 @@
 import os, runpy
 os.environ["TPG_SUB_DESTROYED"] = "0"
 os.environ["TPG_SUB_LOD"] = "2"
-runpy.run_path("edm-jobs/build_tpg_substation.py", run_name="__main__")
+runpy.run_path("edm-jobs/run_tpg_substation_user_asset_build.py", run_name="__main__")
 runpy.run_path("edm-jobs/postprocess_tpg_substation_surface_upgrade.py", run_name="__main__")
 runpy.run_path("edm-jobs/add_tpg_substation_projector_connectors.py", run_name="__main__")
