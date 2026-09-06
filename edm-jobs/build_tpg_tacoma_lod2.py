5dd0623bdb38351e01304638044e009501362cee
Thepropergrip <huntjlc@gmail.com>
2026-09-06 05:41:47 -0400

    Tacoma V28 use canonical body pass only for LOD2

--- edm-jobs/build_tpg_tacoma_lod2.py
+++ edm-jobs/build_tpg_tacoma_lod2.py
@@ -4,5 +4,4 @@ os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="2"
 runpy.run_path("edm-jobs/build_tpg_tacoma.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_canonical_photo_match.py", run_name="__main__")
-runpy.run_path("edm-jobs/build_tpg_tacoma_windshield_stance_v27.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_wheel_closeout.py", run_name="__main__")
