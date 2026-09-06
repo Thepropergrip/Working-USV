cc1f15c0df8dfc677a838445551e12b83d214ef6
Thepropergrip <huntjlc@gmail.com>
2026-09-06 05:01:41 -0400

    Wire Tacoma V27 hero-body pass into LOD1

--- edm-jobs/build_tpg_tacoma_lod1.py
+++ edm-jobs/build_tpg_tacoma_lod1.py
@@ -4,4 +4,5 @@ os.environ["TPG_TACOMA_DESTROYED"]="0"
 os.environ["TPG_TACOMA_LOD"]="1"
 runpy.run_path("edm-jobs/build_tpg_tacoma.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_canonical_photo_match.py", run_name="__main__")
+runpy.run_path("edm-jobs/build_tpg_tacoma_windshield_stance_v27.py", run_name="__main__")
 runpy.run_path("edm-jobs/build_tpg_tacoma_wheel_closeout.py", run_name="__main__")
